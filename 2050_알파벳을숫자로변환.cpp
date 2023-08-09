672a300ba6d4adb41656f3e67e15e430feb2c36b
ChoiYJ2 <chldb1221@naver.com>
2023-08-09 12:08:54 +0900

    D1_2050_알파벳을숫자로변환_C++_pass
    note) mem: 13,540kb, runtime: 5ms

---
+++ 2050_알파벳을숫자로변환.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	string T;
+	cin >> T;
+	for (int i = 0; i < T.size(); i++)
+	{
+		cout << (int)(T[i] - 'A') + 1 << " ";
+	}
+	return 0;
+}
